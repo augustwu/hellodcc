ad52c9ca4745ed4e9c13cc17f155ae4071939212
augustwu <qq263020776@gmail.com>
2018-02-21 12:46:09 +0800

    add test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from django.test import Client
+import json
+
+class HelloWorldViewTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        key="msg"
+        value ="hello-world"
+        key2 = "msg2"
+        value2 = "hello-world2"
+
+        request_dict = {key:value,key2:value2}
+        response = self.client.get('/api/v1/echo/',request_dict)
+        self.assertEqual(response.status_code, 200)
+        resp_dict = json.loads(response.content) 
+
+        self.assertEqual(resp_dict, request_dict)
+
