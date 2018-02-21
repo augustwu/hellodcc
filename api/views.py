ef97d436a8eb680595cbaf87219c74dee0ab650a
augustwu <qq263020776@gmail.com>
2018-02-21 12:49:25 +0800

    add views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
+# Create your views here.
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from urllib.parse import urlsplit, parse_qs
+
+class HelloWorldView(APIView):
+
+
+    def get(self,request):
+        url = request.build_absolute_uri()
+        query = urlsplit(url).query
+        params = parse_qs(query)
+        resp = {k: v[0] for k, v in params.items()}
+
+        return Response(resp)
